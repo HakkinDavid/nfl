a7c4f82599b34741fa92a0afcee929dcc8b83f1b
HakkinDavid <desr11@hotmail.com>
2025-09-25 03:27:53 +0000

    corregir error de duplicado falso positivo en equipos

--- tournament_common/include/persistence/configuration/PostgresConnectionProvider.hpp
+++ tournament_common/include/persistence/configuration/PostgresConnectionProvider.hpp
@@ -23,6 +23,7 @@ public:
         for (size_t i = 0; i < poolSize; i++) {
             connectionPool.push(std::make_unique<pqxx::connection>(connectionString.data()));
             connectionPool.back()->prepare("insert_tournament", "insert into TOURNAMENTS (document) values($1) RETURNING id");
+            connectionPool.back()->prepare("insert_team", "insert into TEAMS (document) values($1) RETURNING id");
         }
     }
 
